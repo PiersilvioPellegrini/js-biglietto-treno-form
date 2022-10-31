2ccbcc019434e7989cad7d61cdc3a92a112551a4
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-10-31 13:46:04 +0100

    COMPLETED

--- JS/ticketForm.js
+++ JS/ticketForm.js
@@ -11,69 +11,79 @@ let sconto;
 let prezzoFinale;
 
 
+// Bottone submit
 submit.addEventListener("click", function () {
-    // VERIFICA VALORI NON FUNZIONANATE
-    if((userEl == "") && (KmEl == "") && (ageEl == "")){
-        alert("inserisci tutti i dati richiesti"); 
+    // VERIFICARE SE I CAMPI SONO RIEMPITI
+  if (userEl.value == "" || KmEl.value == "" || ageEl.value == "") {
+    alert("inserisci tutti i dati richiesti");
+
+    // VERIFICA CHE I KM SIANO UN VALORE NUMERICO E NON UNA STRINGA
+  } else if (isNaN(KmEl.value)) {
+    alert("inserisci un numero valido per i km");
+    
+  }
+  
+  
+  
+  else {
+    // Associo valore alla variabile
+    Utente = userEl.value;
+    console.log(Utente);
+    kmUtente = KmEl.value;
+    console.log(kmUtente);
+    etaUtente = ageEl.value;
+    console.log(etaUtente);
+
+    // CALCOLO PREZZO biglietto
+    prezzoBiglietto = kmUtente * 0.21;
+
+    // CALCOLO SCONTO
+    if (etaUtente == "minorenne") {
+      sconto = prezzoBiglietto * 0.2;
+    } else if (etaUtente == "over65") {
+      sconto = prezzoBiglietto * 0.4;
+    } else {
+      sconto = 0;
     }
-    else {
-        // Associo valore alla variabile
-        Utente = userEl.value;
-        console.log(Utente);
-        kmUtente = KmEl.value;
-        console.log(kmUtente);
-        etaUtente = ageEl.value;
-        console.log(etaUtente);
-        
 
-        
+    // calcolo prezzo finale
+    prezzoFinale = prezzoBiglietto - sconto;
 
-        // CALCOLO PREZZO biglietto 
-         prezzoBiglietto = kmUtente * 0.21;
+    // OUTPUT VALORI UTEENTE
+    const outputName = document.querySelector("#user");
+    outputName.innerHTML = `il tuo nome: ${Utente}`;
 
-        // CALCOLO SCONTO
-        if(etaUtente == "minorenne"){
-            sconto = prezzoBiglietto * 0.2;
-        }else if(etaUtente == "over65"){
-            sconto = prezzoBiglietto * 0.4;
-        }else{
-            sconto = 0;
+    const outputAge = document.querySelector("#categoria");
+    outputAge.innerHTML = `la tua categoria: ${etaUtente}`;
 
-        }
-        
-        // calcolo prezzo finale
-        prezzoFinale = prezzoBiglietto - sconto;
+    const outputPrice = document.querySelector("#price");
+    outputPrice.innerHTML = `il prezzo del tuo biglietto è: ${prezzoFinale.toFixed(
+      2
+    )}€`;
 
-            
-        // OUTPUT VALORI UTEENTE
-        const outputName = document.querySelector("#user");
-        outputName.innerHTML = `il tuo nome: ${Utente}`;
-
-        const outputAge = document.querySelector("#categoria");
-        outputAge.innerHTML = `la tua categoria: ${etaUtente}`;
-
-        const outputPrice = document.querySelector("#price");
-        outputPrice.innerHTML = `il prezzo del tuo biglietto è: ${prezzoFinale.toFixed(2)}€`;
-
-        // IN BASE ALLO SCONTO MESSAGGIO DIVERSO
-        if(sconto>0){
-        const outputDiscount = document.querySelector("#discount");
-        outputDiscount.innerHTML = `lo sconto applicato è: ${sconto.toFixed(2)}€`;
-        }else{
-            const outputDiscount = document.querySelector("#discount");
-            outputDiscount.innerHTML = `non hai diritto a sconti`;
-        }
-
-
-
-    }    
+    // IN BASE ALLO SCONTO MESSAGGIO DIVERSO
+    if (sconto > 0) {
+      const outputDiscount = document.querySelector("#discount");
+      outputDiscount.innerHTML = `lo sconto applicato è: ${sconto.toFixed(2)}€`;
+    } else {
+      const outputDiscount = document.querySelector("#discount");
+      outputDiscount.innerHTML = `non hai diritto a sconti`;
+    }
+  }
 });
 
+
+// Button DELETE
 reset.addEventListener("click", function () {
-    userEl.value = "";
-    KmEl.value = "";
-    ageEl.value = "standard";
-
+  userEl.value = "";
+  KmEl.value = "";
+  ageEl.value = "standard";
+  const outputName = document.querySelector("#user");
+  outputName.innerHTML = ``;
+  const outputAge = document.querySelector("#categoria");
+  outputAge.innerHTML = ``;
+  const outputPrice = document.querySelector("#price");
+  outputPrice.innerHTML = ``;
+  const outputDiscount = document.querySelector("#discount");
+  outputDiscount.innerHTML = ``;
 });
-
-
